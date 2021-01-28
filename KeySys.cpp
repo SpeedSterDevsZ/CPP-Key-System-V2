3e223d4afeb2c09e755c89ecb47be387a3002937
SpeedSterDevs <72403623+SpeedSterDevsZ@users.noreply.github.com>
2021-01-28 09:29:09 -0500

    Create KeySys.cpp

---
+++ KeySys.cpp
@@ -0,0 +1,20 @@
+
+// You need to Paste this inside ur dll exploit source whatever 
+
+void KeySystem()
+{
+
+	using namespace std;
+
+	string key;
+	getline(cin, key);
+
+	if (key == "aFwez6QEcn") // Key
+	{
+		std::cout  << "The Key aFwez6QEcn is Correct.\n";
+	}
+	else 
+	{
+		std::cout  << "The Key is Incorrect Retard.\n";
+	}
+}
